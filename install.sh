6a34e667a8c8144a35a747c3f79cef4a78cba3b1
Andrew Baker <andrew@chronalstudios.com>
2019-09-25 09:28:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd ./Resources
 
-cp -R ./ ~/.local/share/flatpak/app/com.teamspeak.TeamSpeak/x86_64/stable/8d6e0b4c9c92367928b8a29d252197ac163ac4123e32fd1157d054d631518d5a/files/extra
+cp -R ./Resources/ ~/.local/share/flatpak/app/com.teamspeak.TeamSpeak/x86_64/stable/8d6e0b4c9c92367928b8a29d252197ac163ac4123e32fd1157d054d631518d5a/files/extra
 
 exit
